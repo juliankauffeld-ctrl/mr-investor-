fb20087126132a433c28c7553329ee6db9be5494
juliankauffeld-ctrl <julian.kauffeld@gmx.de>
2026-03-17 03:35:03 +0100

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -4,7 +4,7 @@ import { createClient } from '@supabase/supabase-js';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 const supabase = createClient(
   process.env.VITE_SUPABASE_URL,
-  process.env.VITE_SUPABASE_ANON_KEY
+  process.env.SUPABASE_SERVICE_ROLE_KEY
 );
 
 async function buffer(readable) {
